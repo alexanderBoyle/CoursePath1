e5cf524299c1e591c386a8bf2e6d0efdd59a360d
ConsoleFriend <alexander.boyle@student.howest.be>
2022-01-06 22:03:13 +0100

    Added files

---
+++ StartingPlural/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WeatherProgram;
+
+namespace StartingPlural
+{
+    class CheckComfort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Where should we go in May?");
+            Weather.Report("San Francisco", Weather.FahrenheitToCelsius(65), 73);
+            Weather.Report("Denver", Weather.FahrenheitToCelsius(77), 55);
+            Weather.Report("Bologna", 23, 65);
+
+            //Console.WriteLine(FahrenheitToCelsius(80).ToString("0.0"));
+
+            //string[] interestingAnimals = { "Aardvark", "Binturong", "Pangolin" };
+            //string anInterestingAnimal = interestingAnimals[0];
+            //Console.WriteLine(anInterestingAnimal);
+        }
+        
+    }
+}
---
+++ StartingPlural/Weather.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherProgram
+{
+    public class Weather
+    {
+        static public float FahrenheitToCelsius(float temperatureFahrenheit)
+        {
+            return (temperatureFahrenheit - 32) / 1.8f;
+        }
+        static public float CelsiusToFahrenheit(float temperatureCelsius)
+        {
+            return (temperatureCelsius * 1.8f) + 32;
+        }
+
+        //Higher Index less comfort
+        static public float ComfortIndex(float temperatureFahrenheit, float humidityPercent)
+        {
+            //Probaly not a very reliabble formula:
+            return (temperatureFahrenheit + humidityPercent) / 4;
+        }
+        static public void Report(string location, float temperatureCelsius, float humidity)
+        {
+            float temperatureFahrenheit = CelsiusToFahrenheit(temperatureCelsius);
+            Console.WriteLine($"Comfort index for {location}: {ComfortIndex(temperatureFahrenheit, humidity)}");
+        }
+    }
+}
